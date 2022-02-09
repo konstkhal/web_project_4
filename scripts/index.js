b396254bfa860f536d21d8bb363c1110f9c09603
Konstantin Khaletskiy <konstak@yandex.ru>
2022-02-09 08:08:29 +0200

    refactor(html): move cards array into sep-te file
    move an array with the initial cards into a separate file (module) like cards.js and link it to the index.html file before index.js

--- scripts/index.js
+++ scripts/index.js
@@ -8,36 +8,6 @@
 
 const cardsListElement = document.querySelector(".photo-grid__list"); // selecting photo-grid__list to fill with cards
 const cardTemplateElement = document.querySelector("#card-template"); // selecting card template element
-/* -------------------------------------------------------------------------- */
-/*                              Six initial cards                             */
-/* -------------------------------------------------------------------------- */
-
-const initialCards = [
-  {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
-  },
-  {
-    name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg",
-  },
-];
 
 /* -------------------------------------------------------------------------- */
 /*                         Edit profile Functionality                         */
